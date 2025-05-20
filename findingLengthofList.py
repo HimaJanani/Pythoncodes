64359328f1bda3c7f4f25974312d6c3b77cf0d9e
Hima Janani Vinukollu <jananiswag1@gmail.com>
2025-05-20 21:49:30 +0530

    Update findingLengthofList.py

--- findingLengthofList.py
+++ findingLengthofList.py
@@ -1,3 +1,9 @@
+#length of the list
 list = [12, 18, 20, 22, 35, 50]
 length = len(list)
 print("Length of the list:" , length)
+
+#number of elements in a list
+list=[2, 8, 0, 1, 5]
+count = len(list)
+print("number of elements:" , count)
