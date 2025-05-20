3985ee62e83fdaa3930a2b586dc15fbce201f1b4
Hima Janani Vinukollu <jananiswag1@gmail.com>
2025-05-20 22:09:40 +0530

    Add files via upload

---
+++ swappingnumbers.py
@@ -0,0 +1,13 @@
+#swapping numbers
+a = int(input("Enter a number(a):"))
+b = int(input("Enter a number(b):"))
+a,b = b,a
+print("After swapping:")
+print("a:", a)
+print("b:", b)
+
+
+#swapping first and last number in a list
+a = [1, 4, 8, 9, 10]
+a[0] , a[-1] = a[-1] , a[0]
+print(a)
