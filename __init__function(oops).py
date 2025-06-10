fc35aa79ba3a8c2de1e05ea1ed9b120c80308252
Hima Janani Vinukollu <jananiswag1@gmail.com>
2025-06-10 09:46:54 +0530

    Add files via upload

---
+++ __init__function(oops).py
@@ -0,0 +1,19 @@
+class Student:
+    def __init__(self):
+       print("adding new student in Database....")
+s1 = Student()
+print()
+
+
+
+
+class Student:
+    def __init__(self, fullname):
+       self.name = fullname
+       print("adding new student in Database....")
+s1 = Student("Ganesh")
+print(s1.name)
+
+
+
+
