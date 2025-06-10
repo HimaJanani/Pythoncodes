afc874a320d92d9fdaed67bcb52e35396bf6acb7
Hima Janani Vinukollu <jananiswag1@gmail.com>
2025-06-10 09:46:33 +0530

    Add files via upload

---
+++ classAndObjects(OOPS).py
@@ -0,0 +1,31 @@
+#creating class
+class Student:
+    name = "Janani"
+#creating an object/instance/instance of class
+s1 = Student()
+print(s1.name)
+
+
+
+#creating a class
+class Car:
+    color = "red"
+    company = "Mercedes"
+#creating an instance of class
+car1 = Car()
+print(car1.color)
+print(car1.company)
+
+
+
+
+#creating a class
+class Toys:
+    boy = "Mickey Mouse"
+    girl = "Minnie Mouse"
+#creating an instance of class/object/instance
+toys1=Toys()
+print(toys1.boy)
+print(toys1.girl)
+
+
