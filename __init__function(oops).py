ba87767e432e30f7326cdf47f781160d60ea1e5c
Hima Janani Vinukollu <jananiswag1@gmail.com>
2025-06-13 09:29:22 +0530

    Update __init__function(oops).py

--- __init__function(oops).py
+++ __init__function(oops).py
@@ -15,5 +15,10 @@ s1 = Student("Ganesh")
 print(s1.name)
 
 
-
+class Cartoon:
+    def __init__(self,Disney):
+        self.name = Disney
+        print("do you know this cartoon")
+c1 = ("Kit VS Kat")
+print(c1.name)
 
